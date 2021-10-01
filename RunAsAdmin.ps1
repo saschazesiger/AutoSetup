236c6a84743c52c442c608a972f69fbe5b5431c5
saschazesiger <76683226+saschazesiger@users.noreply.github.com>
2021-10-01 23:34:48 +0200

    v7
    RunAsAdmin

---
+++ RunAsAdmin.ps1
@@ -0,0 +1,4 @@
+﻿$wc = New-Object System.Net.WebClient
+if(test-path C:\AutoSetup){}else{mkdir C:\AutoSetup}
+$wc.DownloadFile("https://raw.githubusercontent.com/saschazesiger/AutoSetup/main/AutoSetup.ps1", "C:\AutoSetup\Setup.ps1")
+Start-Process powershell "-command C:\AutoSetup\Setup.ps1" -Verb RunAs
