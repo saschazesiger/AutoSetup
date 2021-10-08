afd8cd72de86032f44c31bfade7ee77d4e1c3d53
saschazesiger <76683226+saschazesiger@users.noreply.github.com>
2021-10-08 09:43:50 +0200

    Update 08.10.21

---
+++ Hintergrund.ps1
@@ -0,0 +1,8 @@
+﻿function Get-Color {
+    $colorDialog = new-object System.Windows.Forms.ColorDialog 
+    $colorDialog.AllowFullOpen = $false
+    [void]$colorDialog.ShowDialog()
+    $colorDialog.Color.Name
+}
+
+get-color
